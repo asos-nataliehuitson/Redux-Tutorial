64209e8dae7fe402a3d39fc6a9f8195f3013abee
Nattie87 <natalie.huitson@gmail.com>
2018-01-14 19:52:02 +0000

    first book reducer complete

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Albert and his Red Balloon' },
+    { title: 'How to be a Woman' },
+    { title: 'Harry Potter' },
+    { title: 'Bible' }
+  ]
+}
